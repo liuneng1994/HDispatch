73c0653b51a399e925b3e1b867bd523e20270532
Yangyazheng <2017948437@qq.com>
2016-09-07 11:27:18 +0800

    新建层

---
+++ core/src/main/java/hdispatch/core/dispatch/dto/layer/Layer.java
@@ -0,0 +1,14 @@
+package hdispatch.core.dispatch.dto.layer;
+
+/**
+ * Created by yyz on 2016/9/7.
+ * yazheng.yang@hand-china.com
+ */
+public class Layer {
+    private long layerId;
+    private String name;
+    private String description;
+    private long themeId;
+    private int seq;
+    private String flowId;
+}
---
+++ core/src/main/java/hdispatch/core/dispatch/mapper/LayerMapper.java
@@ -0,0 +1,11 @@
+package hdispatch.core.dispatch.mapper;
+
+import hdispatch.core.dispatch.dto.layer.Layer;
+
+/**
+ * Created by yyz on 2016/9/7.
+ * yazheng.yang@hand-china.com
+ */
+public interface LayerMapper {
+    void save(Layer layer) throws Exception;
+}
