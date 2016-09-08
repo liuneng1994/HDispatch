d9bb5bd79d4c26ecb21b5643c50138e679e958b6
liuneng1994 <neng.liu@hand-china.com>
2016-09-08 09:09:30 +0800

    模拟组创建

--- core/src/main/webapp/resources/js/dispatch/controller/group/groupCreatingController.js
+++ core/src/main/webapp/resources/js/dispatch/controller/group/groupCreatingController.js
@@ -2,28 +2,35 @@
  * Created by 刘能 on 2016/9/7.
  */
 (function () {
-    angular.module('dispatch').controller('groupCreatingController', ['groupCreatingService', groupCreating]);
-    function groupCreating(groupCreatingService) {
+    angular.module('dispatch').controller('groupCreatingController', ['$scope', 'groupCreatingService', groupCreating]);
+    function groupCreating($scope, groupCreatingService) {
         var vm = this;
+        vm.themes = [{themeName: '加载中'}];
+        vm.layers = [{layerName: '请先选择相应主题'}];
         vm.group = {};
         vm.group.active = true;
         vm.dependencySelectOptions = {
             placeholder: "添加依赖",
-            dataTextField: "ProductName",
-            dataValueField: "ProductID",
+            dataTextField: "groupName",
+            dataValueField: "groupName",
             valuePrimitive: true,
             autoBind: false,
             autoClose: false,
-            dataSource: {
-                type: "odata",
-                serverFiltering: true,
-                transport: {
-                    read: {
-                        url: "//demos.telerik.com/kendo-ui/service/Northwind.svc/Products",
-                    }
-                }
-            }
+            dataSource: [{groupName: '1组'}, {groupName: '2组'}, {groupName: '3组'}]
         };
+        groupCreatingService.getThemes().then(function (data) {
+            vm.themes = data;
+        });
+        vm.refreshLayers = function () {
+            console.log('刷新层');
+            vm.layers = [{layerName: '加载中'}];
+            groupCreatingService.getLayers(vm.group.theme).then(function (data) {
+                vm.layers = data;
+            });
+        }
+        $scope.$watch('vm.group.theme', function () {
+            vm.refreshLayers();
+        });
         return vm;
     };
 
--- core/src/main/webapp/resources/js/dispatch/service/group/groupCreatingService.js
+++ core/src/main/webapp/resources/js/dispatch/service/group/groupCreatingService.js
@@ -2,12 +2,27 @@
  * Created by 刘能 on 2016/9/7.
  */
 (function () {
-    angular.module('dispatch').factory('groupCreatingService', groupCreating);
-    function groupCreating() {
-        return {
-            alert: function () {
+    angular.module('dispatch').factory('groupCreatingService', ['$q', '$http', groupCreating]);
+    function groupCreating($q) {
+        function getThemes() {
+            var defered = $q.defer();
+            var data = [{themeName: '主题1', themeId: 1}, {themeName: '主题2', themeId: 2}, {themeName: '主题3', themeId: 3}]
+            defered.resolve(data);
+            return defered.promise;
+        }
 
-            }
+        function getLayers(theme) {
+            var defered = $q.defer();
+            var data = [[{layerId: 1, layerName: '层1'}, {layerId: 2, layerName: '层2'}, {layerId: 3, layerName: '层3'}],
+                [{layerId: 4, layerName: '层4'}, {layerId: 5, layerName: '层5'}, {layerId: 6, layerName: '层6'}],
+                [{layerId: 7, layerName: '层7'}, {layerId: 8, layerName: '层8'}, {layerId: 9, layerName: '层9'}]]
+            defered.resolve(data[theme - 1]);
+            return defered.promise;
+        }
+
+        return {
+            getThemes: getThemes,
+            getLayers: getLayers
         };
     }
 })()
