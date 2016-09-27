b23c91b6e5f477128a24b92755f50d0f0ecb3f2c
Yangyazheng <2017948437@qq.com>
2016-09-27 16:21:21 +0800

    SVN参数设置-多语言

--- core-db/src/main/java/hdispatch/core/db/2016-06-01-init-table-migration.groovy
+++ core-db/src/main/java/hdispatch/core/db/2016-06-01-init-table-migration.groovy
@@ -39,4 +39,20 @@ databaseChangeLog(logicalFilePath: "hdispatch/core/db/2016-06-01-init-migration.
         sqlFile(path: MigrationHelper.getInstance().dataPath("hdispatch/core/db/data/mysql/tables/hdispatch_layer.sql"), encoding: "UTF-8")
     }
 
+
+//    changeSet(author: "yangyazheng", id: "20160927-yangyazheng-1") {
+//        createTable(tableName: "hdispatch_fnd_schedule_parameter") {
+//            column(autoIncrement: "true", name: "id", type: "BIGINT") {
+//                constraints(primaryKey: "true")
+//            }
+//            column(name: "group_name", type: "VARCHAR(128)")
+//            column(name: "layer_id", type: "BIGINT")
+//            column(name: "schedule_expression", type: "VARCHAR(256)")
+//            column(name: "flow_id", type: "VARCHAR(128)")
+//            column(name: "active", type: "TINYINT")
+//            column(name: "create_at", type: "DATETIME")
+//            column(name: "update_at", type: "DATETIME")
+//        }
+//    }
+
 }
