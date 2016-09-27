2422b6713c4570b879667054e7dd4f016eb61c01
Yangyazheng <2017948437@qq.com>
2016-09-27 14:46:01 +0800

    SVN参数excel批量导入控制器实现

--- core/src/main/java/hdispatch/core/dispatch/controllers/SvnParameterController.java
+++ core/src/main/java/hdispatch/core/dispatch/controllers/SvnParameterController.java
@@ -1,6 +1,7 @@
 package hdispatch.core.dispatch.controllers;
 
 import com.hand.hap.core.IRequest;
+import com.hand.hap.core.annotation.StdWho;
 import com.hand.hap.system.controllers.BaseController;
 import com.hand.hap.system.dto.ResponseData;
 import hdispatch.core.dispatch.dto.svn.SvnParameter;
@@ -10,9 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import org.springframework.web.servlet.support.RequestContextUtils;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.*;
 import java.util.List;
 import java.util.Locale;
 
@@ -72,7 +75,7 @@ public class SvnParameterController extends BaseController{
      */
     @RequestMapping(value = "/dispatcher/svnParameter/submit", method = RequestMethod.POST, consumes = "application/json")
     @ResponseBody
-    public ResponseData addSvnParameters(@RequestBody List<SvnParameter> svnParameterList, BindingResult result, HttpServletRequest request) {
+    public ResponseData addSvnParameters(@RequestBody @StdWho List<SvnParameter> svnParameterList, BindingResult result, HttpServletRequest request) {
 
         ResponseData rd = null;
         //后台验证
@@ -116,6 +119,7 @@ public class SvnParameterController extends BaseController{
             logger.error(errorMsg, e);
             rd = new ResponseData(false);
             rd.setMessage(errorMsg);
+            e.printStackTrace();
             return rd;
         }
         return rd;
@@ -156,7 +160,7 @@ public class SvnParameterController extends BaseController{
 
     @RequestMapping(value = "/dispatcher/svnParameter/update", method = RequestMethod.POST, consumes = "application/json")
     @ResponseBody
-    public ResponseData updateJobs(@RequestBody List<SvnParameter> svnParameterList, BindingResult result, HttpServletRequest request) {
+    public ResponseData updateSvnParameters(@RequestBody List<SvnParameter> svnParameterList, BindingResult result, HttpServletRequest request) {
 
         ResponseData rd = null;
         //后台验证
@@ -168,6 +172,26 @@ public class SvnParameterController extends BaseController{
         }
         IRequest requestContext = createRequestContext(request);
         rd = new ResponseData(svnParameterService.batchUpdate(requestContext,svnParameterList));
+        return rd;
+    }
+
+
+    @RequestMapping(value = "/dispatcher/svnParameter/importFromExcel",method = RequestMethod.POST)
+    public ResponseData addSvnParameters(@RequestParam("excelFiles") CommonsMultipartFile[] files, HttpServletRequest req){
+
+        List<SvnParameter> svnParameterList = null;
+        ResponseData rd = null;
+        try {
+            svnParameterList = svnParameterService.batchCreateFromExcel(files);
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("从Excel中批量导入SVN 参数中途出错",e);
+            rd = new ResponseData(false);
+            rd.setMessage("从Excel中批量导入SVN 参数中途出错");
+            return rd;
+        }
+
+        rd = new ResponseData(svnParameterList);
         return rd;
     }
 }
--- core/src/main/java/hdispatch/core/dispatch/service/SvnParameterService.java
+++ core/src/main/java/hdispatch/core/dispatch/service/SvnParameterService.java
@@ -1,7 +1,9 @@
 package hdispatch.core.dispatch.service;
 
 import com.hand.hap.core.IRequest;
+import com.hand.hap.core.annotation.StdWho;
 import hdispatch.core.dispatch.dto.svn.SvnParameter;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import java.util.List;
 
@@ -25,5 +27,12 @@ public interface SvnParameterService {
     /**
      * 批量操作：插入、删除、更新
      */
-    List<SvnParameter> batchUpdate(IRequest requestContext, List<SvnParameter> svnParameterList);
+    List<SvnParameter> batchUpdate(IRequest requestContext, @StdWho List<SvnParameter> svnParameterList);
+
+    /**
+     * 从excel文件中导入数据，批量创建 SVN参数
+     * @param excelFiles
+     * @return
+     */
+    List<SvnParameter> batchCreateFromExcel(CommonsMultipartFile[] excelFiles) throws Exception;
 }
