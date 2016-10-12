d9988d6c415285921bed6ce8ee72e0360ea9dae0
10890 <zhilong.deng@hand-china.com>
2016-10-12 16:38:56 +0800

    进度条修改

--- core/src/main/java/hdispatch/core/dispatch/controllers/ExecutionFlowsController.java
+++ core/src/main/java/hdispatch/core/dispatch/controllers/ExecutionFlowsController.java
@@ -67,18 +67,17 @@ public class ExecutionFlowsController extends BaseController {
 			ex.setExec_id(e.getExec_id());
 			ex.setFlow_id(e.getFlow_id());
 			ex.setLang(irequest.getLocale());
-			PageHelper.startPage(page, pagesize);
 			List<ExecutionJobs> list2 = exeJobsService.selectJobsByFlow(ex);
 			for (ExecutionJobs job : list2) {
 				if(job.getStatus()==50)
 					count++;
 			}
-			System.out.println(count);
 			if(list2.size()!=0)
 			d=Math.ceil(count/list2.size());
 			else
 			d=0;
 			e.setRunning((new Double(d)).intValue()*100);
+			System.out.println(count+"----"+list2.size()+"-----"+d+"------------"+e.getRunning());
 		}
 		return new ResponseData(list);
 	}
