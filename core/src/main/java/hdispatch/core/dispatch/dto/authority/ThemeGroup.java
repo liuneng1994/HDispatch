cd2cedbea761e2ea7ad54f8c467c6126cfccdefe
Yangyazheng <2017948437@qq.com>
2016-10-17 10:31:41 +0800

    主题组

---
+++ core/src/main/java/hdispatch/core/dispatch/dto/authority/ThemeGroup.java
@@ -0,0 +1,43 @@
+package hdispatch.core.dispatch.dto.authority;
+
+import com.hand.hap.system.dto.BaseDTO;
+
+/**
+ * Created by yyz on 2016/10/14.
+ *
+ * @author yazheng.yang@hand-china.com
+ *
+ * 主题组
+ */
+public class ThemeGroup extends BaseDTO {
+    private Long themeGroupId;
+    private String themeGroupName;
+    private String themeGroupDesc;
+
+    public Long getThemeGroupId() {
+        return themeGroupId;
+    }
+
+    public ThemeGroup setThemeGroupId(Long themeGroupId) {
+        this.themeGroupId = themeGroupId;
+        return this;
+    }
+
+    public String getThemeGroupName() {
+        return themeGroupName;
+    }
+
+    public ThemeGroup setThemeGroupName(String themeGroupName) {
+        this.themeGroupName = themeGroupName;
+        return this;
+    }
+
+    public String getThemeGroupDesc() {
+        return themeGroupDesc;
+    }
+
+    public ThemeGroup setThemeGroupDesc(String themeGroupDesc) {
+        this.themeGroupDesc = themeGroupDesc;
+        return this;
+    }
+}
