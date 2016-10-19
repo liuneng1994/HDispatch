29daff009de88c8f9fa21e7e396a5c1e5132234a
Yangyazheng <2017948437@qq.com>
2016-10-19 09:39:20 +0800

    ThemeGroupThemeMapper.xml查询和新建

--- core/src/main/java/hdispatch/core/dispatch/dto/authority/ThemeGroupTheme.java
+++ core/src/main/java/hdispatch/core/dispatch/dto/authority/ThemeGroupTheme.java
@@ -13,7 +13,37 @@ public class ThemeGroupTheme extends BaseDTO {
     private Long themeGroupThemeId;
     private Long themeGroupId;
     private Long themeId;
+    //冗余字段
+    private String themeName;
+    private String themeDescription;
+    private String themeGroupName;
 
+    public String getThemeName() {
+        return themeName;
+    }
+
+    public ThemeGroupTheme setThemeName(String themeName) {
+        this.themeName = themeName;
+        return this;
+    }
+
+    public String getThemeDescription() {
+        return themeDescription;
+    }
+
+    public ThemeGroupTheme setThemeDescription(String themeDescription) {
+        this.themeDescription = themeDescription;
+        return this;
+    }
+
+    public String getThemeGroupName() {
+        return themeGroupName;
+    }
+
+    public ThemeGroupTheme setThemeGroupName(String themeGroupName) {
+        this.themeGroupName = themeGroupName;
+        return this;
+    }
 
     public Long getThemeGroupThemeId() {
         return themeGroupThemeId;
