57ca3ff31a76206c84d4e7f3498cc380e4f87adc
Yangyazheng <2017948437@qq.com>
2016-10-26 10:22:53 +0800

    权限验证函数--version 2.0

--- core-db/src/main/java/hdispatch/core/db/data/mysql/tables/hdispatch_permission_function.sql
+++ core-db/src/main/java/hdispatch/core/db/data/mysql/tables/hdispatch_permission_function.sql
@@ -50,7 +50,7 @@ BEGIN
 		END IF;
 	END IF;
 END;
--- 测试
+-- 测试和使用示例
 SELECT hasPermission(1,1,'Y','Y');
 SELECT hasPermission(1,1,'Y','N');
 SELECT hasPermission(2,1,'Y','Y');
@@ -65,3 +65,53 @@ WHERE "Y"=hasPermission(1,1,'Y','Y');
 
 SELECT * FROM hdispatch_theme
 WHERE 'Y'=hasPermission(1,1,'Y','N');
+
+
+-- 权限判定函数
+-- version 2.0
+-- 修复version 1.1中可能出现的一个用户对同一个主题有多个权限组合时，多对一的错误
+-- 参数说明：in_theme_id       bigint        传入的主题id
+-- 参数说明：in_user_id        bigint        传入的用户id
+-- 参数说明：in_auth_read      VARCHAR(1)    传入的读权限：'Y'、'N'、NULL，其中为NULL表示不需要检查此权限(不等同于'N')
+-- 参数说明：in_auth_operate   VARCHAR(1)    传入的操作权限：'Y'、'N'、NULL，其中为NULL表示不需要检查此权限(不等同于'N')
+DROP FUNCTION IF EXISTS `hasPermission`;
+
+CREATE FUNCTION hasPermission(in_theme_id bigint,in_user_id bigint,in_auth_read VARCHAR(1),in_auth_operate VARCHAR(1))
+	RETURNS VARCHAR(1)
+BEGIN
+	DECLARE has_read_str VARCHAR(1);
+	DECLARE has_operate_str VARCHAR(1);
+
+	IF(in_auth_read IS NULL AND in_auth_operate IS NULL) THEN RETURN 'N';
+	END IF;
+
+	IF(in_auth_read IS NOT NULL AND in_auth_operate IS NOT NULL) THEN
+		SELECT auth_read,auth_operate
+		INTO has_read_str,has_operate_str
+		FROM hdispatch_authority auth JOIN hdispatch_themegroup_theme relation
+		ON auth.theme_group_id = relation.theme_group_id
+		WHERE user_id=in_user_id AND theme_id=in_theme_id AND auth_read=in_auth_read AND auth_operate=in_auth_operate;
+		IF(has_read_str IS NOT NULL AND has_operate_str IS NOT NULL) THEN RETURN 'Y';END IF;
+		RETURN 'N';
+	END IF;
+
+	IF(in_auth_read IS NOT NULL) THEN
+		SELECT auth_read,auth_operate
+		INTO has_read_str,has_operate_str
+		FROM hdispatch_authority auth JOIN hdispatch_themegroup_theme relation
+		ON auth.theme_group_id = relation.theme_group_id
+		WHERE user_id=in_user_id AND theme_id=in_theme_id AND auth_read=in_auth_read;
+		IF(has_read_str IS NOT NULL) THEN RETURN 'Y'; END IF;
+		RETURN 'N';
+	END IF;
+
+	IF(in_auth_operate IS NOT NULL) THEN
+		SELECT auth_read,auth_operate
+		INTO has_read_str,has_operate_str
+		FROM hdispatch_authority auth JOIN hdispatch_themegroup_theme relation
+		ON auth.theme_group_id = relation.theme_group_id
+		WHERE user_id=in_user_id AND theme_id=in_theme_id AND auth_operate=in_auth_operate;
+		IF(has_operate_str IS NOT NULL) THEN RETURN 'Y'; END IF;
+		RETURN 'N';
+	END IF;
+END;
