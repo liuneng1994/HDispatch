aaa6483de1f01b00bcecf898cfa2bab1dfcb32e7
Yangyazheng <2017948437@qq.com>
2016-10-27 12:18:33 +0800

    主题权限控制mapper

--- core/src/main/java/hdispatch/core/dispatch/mapper/ThemeMapper.java
+++ core/src/main/java/hdispatch/core/dispatch/mapper/ThemeMapper.java
@@ -19,27 +19,34 @@ public interface ThemeMapper {
     void save(Theme theme) throws Exception;
 
     /**
-     * 查询所有活跃主题
+     * 查询所有活跃主题(只查询那些可以操作的主题)
      * @return
      */
-    List<Theme> selectAll();
+    List<Theme> selectAll_opt();
 
     /**
-     * 通过theme进行查询(多条件，支持模糊查询)
+     * 查询所有活跃主题(只查询那些需要操作权限、读权限的主题)
+     * @return
+     */
+    List<Theme> selectAll_read();
+
+    /**
+     * 通过theme进行查询(多条件，支持模糊查询)<br>
+     *     (查询那些可以读、操作的主题)
      * @param theme
      * @return
      */
     List<Theme> selectByTheme(Theme theme);
 
     /**
-     * 通过主题名称查询活跃的主题
+     * 通过主题名称查询活跃的主题（用于创建时查重）
      * @param theme
      * @return
      */
     Theme selectByNameAndActive(Theme theme);
 
     /**
-     * 逻辑删除
+     * 逻辑删除（需要验证操作权限）
      * @param theme
      */
     void deleteInLogic(Theme theme);
@@ -50,48 +57,4 @@ public interface ThemeMapper {
      * @return
      */
     Theme selectById(Theme theme);
-
-    //--------------------权限验证-----------------------
-    /**
-     * 权限验证下：查询所有主题
-     * @param userId
-     * @return
-     */
-    List<Theme> selectAll_validated(@Param("userId") Long userId);
-
-    /**
-     *  权限验证下：主题模糊搜索，根据权限，过滤掉不可见的主题,<br>
-     *  通过theme进行查询(多条件，支持模糊查询)
-     * @param themeId
-     * @param userId
-     * @param themeName
-     * @param themeDescription
-     * @return
-     */
-    List<Theme> selectByTheme_validated(@Param("themeId") Long themeId, @Param("userId") Long userId,@Param("themeName") String themeName,@Param("themeDescription") String themeDescription);
-
-    /**
-     * 权限验证下：主题搜索(根据themeName选择active的主题)，根据权限，过滤掉不可见的主题<br>
-     *     通过主题名称查询活跃的主题
-     * @param themeName
-     * @param userId
-     * @return
-     */
-    Theme selectByNameAndActive_validated(@Param("themeName")String themeName,@Param("userId")Long userId);
-
-    /**
-     * 权限验证下：删除主题<br>
-     *     逻辑删除
-     * @param themeId
-     * @param userId
-     */
-    void deleteInLogic_validated(@Param("themeId")Long themeId,@Param("userId")Long userId);
-
-    /**
-     * 权限验证下：根据id查询主题
-     * @param themeId
-     * @param userId
-     * @return
-     */
-    Theme selectById_validated(@Param("themeId")Long themeId,@Param("userId")Long userId);
 }
