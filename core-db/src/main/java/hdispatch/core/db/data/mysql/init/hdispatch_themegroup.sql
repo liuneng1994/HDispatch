d0cdf6287d6d366b01d3e1b7d6f9e422adabdec6
Yangyazheng <2017948437@qq.com>
2016-11-07 18:06:25 +0800

    主题组表创建以及数据初始化脚本

---
+++ core-db/src/main/java/hdispatch/core/db/data/mysql/init/hdispatch_themegroup.sql
@@ -0,0 +1,4 @@
+-- 主题组数据（这些数据不可删除）
+INSERT INTO `hdispatch_themegroup` (`theme_group_id`, `theme_group_name`, `theme_group_desc`) VALUES ('1001', '主题组1', '');
+INSERT INTO `hdispatch_themegroup` (`theme_group_id`, `theme_group_name`, `theme_group_desc`) VALUES ('1002', 'job_runtime_parameters_management', '管理任务运行时参数的权限（本主题组的名称约定不变）');
+INSERT INTO `hdispatch_themegroup` (`theme_group_id`, `theme_group_name`, `theme_group_desc`) VALUES ('1003', 'theme_layer_management', '管理主题和层次页面权限（本主题组的名称约定不变）');
