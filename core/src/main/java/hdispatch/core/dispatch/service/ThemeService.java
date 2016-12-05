430017f58778d48f380d0b2c0353cfc4573f0741
Yangyazheng <2017948437@qq.com>
2016-12-05 09:30:12 +0800

    主题适配乐观锁

--- core/src/main/java/hdispatch/core/dispatch/service/ThemeService.java
+++ core/src/main/java/hdispatch/core/dispatch/service/ThemeService.java
@@ -2,16 +2,19 @@ package hdispatch.core.dispatch.service;
 
 import com.hand.hap.core.IRequest;
 import com.hand.hap.core.ProxySelf;
+import com.hand.hap.core.annotation.StdWho;
+import com.hand.hap.system.service.IBaseService;
 import hdispatch.core.dispatch.dto.theme.Theme;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * 主题service接口<br>
  * Created by yyz on 2016/9/5.
  * yazheng.yang@hand-china.com
  */
-public interface ThemeService extends ProxySelf<ThemeService> {
+public interface ThemeService extends IBaseService<Theme>, ProxySelf<ThemeService> {
     /**
      * 根据主题模糊查询（当前用户可见的）主题列表
      * @param requestContext
@@ -38,21 +41,13 @@ public interface ThemeService extends ProxySelf<ThemeService> {
 
 
     /**
-     * 批量编辑（目前只是新增和删除，删除没有检查主题下是否有层次）
+     * 批量编辑（新增、更新和删除，删除没有检查主题下是否有层次）
      * @param requestContext
      * @param themeList
      * @return
      * @throws Exception
      */
-    List<Theme> batchUpdate(IRequest requestContext,List<Theme> themeList) throws Exception;
-
-    /**
-     * 检查数据库中是否存在处于生效(active)状态，主题名称相同，
-     * 用于新增主题之前检测是否已经有同名的主题存在
-     * @param themeList
-     * @return
-     */
-    boolean[] checkIsExist(List<Theme> themeList);
+    List<Theme> batchUpdate(IRequest requestContext, @StdWho List<Theme> themeList, Map<String,String> feedbackMsg);
 
     /**
      * 逻辑删除主题
@@ -61,19 +56,12 @@ public interface ThemeService extends ProxySelf<ThemeService> {
     void deleteInLogic(Theme theme);
 
     /**
-     * 根据id查找active(没被删除)的主题
-     * @param theme
-     * @return
-     */
-    Theme selectActiveThemeById(Theme theme);
-
-    /**
      * 获取传入的列表中没有挂载层次的主题（用于删除之前的检查）
      * @param requestContext
      * @param themeList
      * @return
      */
-    List<Theme> checkIsMountThemes(IRequest requestContext, List<Theme> themeList);
+    List<Theme> checkIsMountThemes(IRequest requestContext, @StdWho List<Theme> themeList);
 
     /**
      * 判断当前用户是否有操作主题的权限<br>
@@ -83,11 +71,9 @@ public interface ThemeService extends ProxySelf<ThemeService> {
     boolean hasOperatePermission(IRequest requestContext);
 
     /**
-     * 批量更新主题<br>
-     *     在更新之前检查是否会引起主题名称重复，将会引起重复的主题放置在duplicateThemesReturn返回
-     * @param requestContext
-     * @param themesToModify
-     * @return List<Theme> duplicateThemesReturn——返回的重复主题列表
+     * 判断主题下是否挂载层次
+     * @param theme
+     * @return true:挂载层次；false:没有挂载层次
      */
-    List<Theme> batchModify(IRequest requestContext, List<Theme> themesToModify);
+    boolean isLayersUnderTheme(Theme theme);
 }
