71d88cc79c93d31a00cfde76d0d644321dc76a27
Yangyazheng <2017948437@qq.com>
2017-01-13 16:04:07 +0800

    fix：日志清理传值错误

--- core/src/main/java/hdispatch/core/dispatch/mapper_hdispatch/ExecutionJobsMapper.java
+++ core/src/main/java/hdispatch/core/dispatch/mapper_hdispatch/ExecutionJobsMapper.java
@@ -28,7 +28,7 @@ public interface ExecutionJobsMapper {
     /**
      * 清空日志,
      * add by yazheng.yang
-     * @param dateBefore
+     * @param dateBeforeMillisecond
      */
-    void cleanLogsBefore(@Param("dateBefore") Date dateBefore);
+    void cleanLogsBefore(@Param("dateBefore") Long dateBeforeMillisecond);
 }
